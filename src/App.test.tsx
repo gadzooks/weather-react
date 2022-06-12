7a83e4d8ad468d031526980b8219290daf05f5ae
akarwande <akarwande@coredigital.com>
2022-06-12 14:06:42 -0700

    react typescript tests working !

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
+import React from 'react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('show loading when waiting for data', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/loading/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/utils/icon.test.tsx
+++ src/utils/icon.test.tsx
@@ -3,6 +3,6 @@ import { icon_class } from "./icon"
 describe('icon_class', () => {
     it('should show mapping for clear-day', () => {
         const result = icon_class('clear-day', 0, 0, 0);
-        expect(result).toStrictEqual('');
+        expect(result).toStrictEqual('wi weather-icon wi-day-sunny day-sunny ');
     })
 })
