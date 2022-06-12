b192ce4e5bb48949488dd9c7d403436d200baf1e
akarwande <akarwande@coredigital.com>
2022-06-12 14:21:42 -0700

    show icons with colors

--- src/utils/icon.test.tsx
+++ src/utils/icon.test.tsx
@@ -5,4 +5,15 @@ describe('icon_class', () => {
         const result = icon_class('clear-day', 0, 0, 0);
         expect(result).toStrictEqual('wi weather-icon wi-day-sunny day-sunny ');
     })
+
+    it('should show mapping for cloudy', () => {
+        const result = icon_class('cloudy', 10, 0, 0);
+        expect(result).toStrictEqual('wi weather-icon wi-day-cloudy day-cloudy sunshine-10');
+    })
+
+    it('should show mapping for cloudy', () => {
+        const result = icon_class('cloudy', 30, 0, 0);
+        expect(result).toStrictEqual('wi weather-icon wi-day-cloudy day-cloudy sunshine-50');
+    })
+
 })
--- src/weather/Location.js
+++ src/weather/Location.js
@@ -2,6 +2,7 @@ import TableCell from '@mui/material/TableCell';
 import TableRow from '@mui/material/TableRow';
 import '../css/weather-icons.css'
 import {icon_class} from '../utils/icon';
+import './Location.scss'
 
 function Location(props) {
   const locationId = props.id;
@@ -14,7 +15,7 @@ function Location(props) {
       {forecasts.map((d, index) => {
           let icon = 'wi';
           if(d.icon) {
-            icon = icon_class(d.icon, d.precipAmount, d.cloudCover, d.maxtemp)
+            icon = icon_class(d.icon, d.precip, d.cloudcover, d.tempmax)
           }
           return <TableCell key={index} className='weather-cell center'><i className={icon}></i></TableCell>
       })}
