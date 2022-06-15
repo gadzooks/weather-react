51c6001fdb1c0cd861b7e1abfa5b94be37c955fb
akarwande <akarwande@coredigital.com>
2022-06-14 21:52:05 -0700

    refactor all components to use typscript

---
+++ src/weather/WeatherIcon.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../css/weather-icons.css'
+import DailyForecastInterface from '../interfaces/DailyForecastInterface';
+import {icon_class} from '../utils/icon';
+
+export function WeatherIcon(props: DailyForecastInterface) {
+    let icon = 'wi';
+    const forecast = props;
+
+    if (forecast.icon) {
+        // console.log(d);
+        icon = icon_class(forecast.icon, forecast.precip, forecast.cloudcover, forecast.tempmax)
+    }
+
+    return (
+        <i className={icon} title={forecast.description}></i>
+    )
+}
+
+export default WeatherIcon;
