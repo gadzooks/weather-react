da1788295736b87b6198d37904a9b81ede20b145
akarwande <akarwande@coredigital.com>
2023-02-24 20:22:35 -0800

    basic recharts works

--- src/components/weather/location_details/LocationDetailChart.tsx
+++ src/components/weather/location_details/LocationDetailChart.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import {
   ComposedChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Bar, LabelList, ReferenceLine,
@@ -30,13 +31,27 @@ export interface LocationDetailChartProps {
   forecastDates: ForeacastDates;
 }
 
+function precipitation(d:DailyForecastInterface):(number | null) {
+  if (d.precip === undefined) {
+    return null;
+  }
+  const round = Math.round(d.precip);
+  return round === 0 ? null : d.precip;
+}
+
+function precipLabel(d:any):(string | null) {
+  const precip = d?.precip;
+  if (precip === null || precip === undefined) {
+    return null;
+  }
+  return precip <= 0.5 ? null : `${precip}"`;
+}
+
 function LocationDetailChart(props: LocationDetailChartProps) {
   const { forecast, forecastDates } = props;
   console.log(forecastDates);
   const data:any = getDataFromForecast(forecast, forecastDates);
-  console.log('--------------');
-  console.log(data);
-  console.log('--------------');
+  // console.log(data);
   // {
   //   name: 'Page A',
   //   tempMax: 4000,
@@ -59,13 +74,12 @@ function LocationDetailChart(props: LocationDetailChartProps) {
         <CartesianGrid strokeDasharray='3 3' />
         <XAxis dataKey='name' name='day' label={{ value: 'Days', position: 'insideBottomRight', offset: 0 }} />
         <YAxis name='Temp' yAxisId='left' label={{ value: 'Temp', angle: -90, position: 'insideLeft' }} domain={[0, (dataMax:number) => (Math.max(dataMax, 100))]} />
-        <YAxis name='Precip' yAxisId='right' orientation='right' />
+        <YAxis name='Precip' yAxisId='right' orientation='right' label={{ value: 'Precipitation', angle: -90, position: 'outsideRight' }} style={{ fontSize: '0.8rem', fontFamily: 'Arial' }} />
         <ReferenceLine y={95} yAxisId='left' label='95' stroke='red' strokeDasharray='3 3' />
         <Tooltip />
         <Legend />
-        <Bar dataKey='precip' fill='rgb(135 201 249)' yAxisId='right' strokeWidth={1}>
-          { /* if precip below 0.1 set it to null */}
-          <LabelList dataKey='precip' position='outside' />
+        <Bar dataKey={(d) => precipitation(d)} fill='rgb(135 201 249)' yAxisId='right' strokeWidth={1}>
+          <LabelList dataKey={(d) => precipLabel(d)} position='outside' />
         </Bar>
         <Line type='monotone' dataKey='tempmax' stroke='#ffb412' yAxisId='left' />
         <Line type='monotone' dataKey='tempmin' stroke='#8884d8' yAxisId='left' activeDot={{ r: 4 }} />
