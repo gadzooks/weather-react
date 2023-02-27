1e3c861395fa3361c1b4dad7cc7b496454471668
akarwande <akarwande@coredigital.com>
2023-02-26 20:10:12 -0800

    simple routing working

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
 import { App } from './App';
 import reportWebVitals from './reportWebVitals';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    // loader: rootLoader,
+    children: [
+      {
+        path: 'team',
+        element: <App />,
+        // loader: teamLoader,
+      },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root') as Element);
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
 
