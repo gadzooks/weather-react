d4cb3c5c47ffef0450d299961ed3e852ba016203
akarwande <akarwande@coredigital.com>
2023-03-10 00:10:53 -0800

    vitest working

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
-import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react'
-import svgrPlugin from 'vite-plugin-svgr'
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
+import reactRefresh from '@vitejs/plugin-react';
+import svgrPlugin from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  test: {
+    globals: true,
+    // ...
+  },
   // This changes the out put dir from dist to build
   // comment this out if that isn't relevant for your project
   build: {
@@ -18,4 +23,4 @@ export default defineConfig({
       },
     }),
   ],
-})
+});
