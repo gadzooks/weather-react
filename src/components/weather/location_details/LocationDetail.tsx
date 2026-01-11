6fc73c8cd2a1bd2bf77f8ab4805d11e3d6063fef
Amit Karwande <karwande@gmail.com>
2026-01-11 12:23:01 -0800

    move github workflows around

--- src/components/weather/location_details/LocationDetail.tsx
+++ src/components/weather/location_details/LocationDetail.tsx
@@ -67,7 +67,6 @@ function LocationDetail(props: LocationDetailProps) {
     setForecastDetailsForLocation,
     forecastDates,
     alertsById,
-    allAlertIds,
   } = props;
 
   const { description, alertIds } = location;
